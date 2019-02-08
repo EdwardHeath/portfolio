69c59f739374cd9afd48e518246f65a80b479204
Edward Heath <e42291+github@gmail.com>
2019-02-07 20:10:53 -0800

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,12 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-134153669-1",
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
